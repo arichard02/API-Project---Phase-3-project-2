84d2bedc607c9d43f0de02b7d95739932b0f8b27
A <arichard02@aol.com>
2022-07-06 19:16:14 -0400

    commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const baseURL = 'https://picsum.photos/v2/list?limit=100';
+
+const App = document.querySelector('#app');
+
+const fetchPhotos = async () => {
+  try {
+    const response = await fetch(baseURL);
+    const photos = await response.json();
+    const randomPhoto = await photos[Math.floor(Math.random() * photos.length)];
+    const photoUI = `
+          <div class="card">
+            <img src="${randomPhoto.download_url}" alter="picsum photo" loading="lazy">
+            <h2><span>Author name :</span> ${randomPhoto.author}</h2>
+            <h4>Photo id : ${randomPhoto.id}</h4>
+          </div>
+        `;
+    App.innerHTML = photoUI;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+
+
+fetchPhotos();
